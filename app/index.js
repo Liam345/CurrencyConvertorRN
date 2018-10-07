4883f4d5bd7bfcb0cbb9d38afb3715a7b73a648f
Liam Banga <liam205@outlook.com>
2018-10-07 16:54:44 +1100

    Added logo

--- app/index.js
+++ app/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import Home from './screens/Home';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Lets builsd a currency convertor!</Text>
-      </View>
-    );
-  }
-}
- 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+EStyleSheet.build({
+    $primaryBlue:'#4F607A',
+    $white: "#FFFFFF"
 });
+
+export default () => <Home />;
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Lets builsd a currency convertor!</Text>
+//       </View>
+//     );
+//   }
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
---
+++ app/screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Container } from '../components/Container';
+import { Logo } from "../components/Logo";
+
+export default () => (
+  <Container>
+    <Logo/>
+    <View />
+  </Container>
+);
