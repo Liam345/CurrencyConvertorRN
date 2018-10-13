cddb5d64b1f18820acf143849426aa672a3b4817
Liam Banga <liam205@outlook.com>
2018-10-13 17:01:34 +1100

    Added ListItem component

--- app/index.js
+++ app/index.js
@@ -1,35 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
-// import Home from './screens/Home';
 import CurrencyList from './screens/CurrencyList';
 
 EStyleSheet.build({
   $primaryBlue: '#4F607A',
   $white: '#FFFFFF',
   $lightGray: '#F0F0F0',
-  $border: '#979797',
+  $border: '#E2E2E2',
   $inputText: '#797979',
+  $darkText: '#343434',
 });
 
-// export default () => <Home />;
 export default () => <CurrencyList />;
-
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Lets builsd a currency convertor!</Text>
-//       </View>
-//     );
-//   }
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import { FlatList, Text, StatusBar, View } from 'react-native';
+import { FlatList, StatusBar, View } from 'react-native';
 import currencies from '../data/currencies';
+import { ListItem, Separator } from '../components/List';
+
+const TEMP_CURRENT_CURRENCY = 'CAD';
+
 class CurrencyList extends Component {
   handlePress = () => {
     console.log('row press');
   };
+
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <StatusBar translucent={false} barStyle="default" />
+        <StatusBar translucent={false} barStyle="light-content" />
         <FlatList
           data={currencies}
-          renderItem={({ item }) => <Text>{item}</Text>}
+          renderItem={({ item }) => (
+            <ListItem
+              text={item}
+              selected={item === TEMP_CURRENT_CURRENCY}
+              onPress={this.handlePress}
+            />
+          )}
+          ItemSeparatorComponent={Separator}
           keyExtractor={item => item}
         />
       </View>
