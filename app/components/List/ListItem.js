93ad912a263436f054fb41a2387c70c8b3f7df72
Liam Banga <liam205@outlook.com>
2018-10-20 17:38:00 +1100

    Added the Themes screen

--- app/components/List/ListItem.js
+++ app/components/List/ListItem.js
@@ -10,11 +10,14 @@ import Icon from './Icon';
     selected = false,
     visible = true,
     customIcon = null,
+    iconBackground,
 }) => (
   <TouchableHighlight onPress={onPress} underlayColor={styles.$underlayColor}>
     <View style={styles.row}>
       <Text style={styles.text}>{text}</Text>
-      {selected ? <Icon visible={visible} checkmark={checkmark} /> : <Icon />}
+      {selected
+        ? <Icon visible={visible} checkmark={checkmark} iconBackground={iconBackground} />
+        : <Icon />}
       {customIcon}
     </View>
   </TouchableHighlight>
--- app/index.js
+++ app/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import Options from './screens/Options';
+import Themes from './screens/Themes';
 
 EStyleSheet.build({
   $primaryBlue: '#4F607A',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
   $white: '#FFFFFF',
   $lightGray: '#F0F0F0',
   $border: '#E2E2E2',
   $inputText: '#797979',
-  $darkText: '#343434',
+  $darkText: '#343434', 
 });
 
-export default () => <Options />;
+export default () => <Themes />;
